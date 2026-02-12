e3694e924d75c7654c2da87dcc22dba1921e3340
sasa12366 <54961843+sasa12366@users.noreply.github.com>
2026-02-12 16:57:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a;
+        a=input.nextInt();
+        double b=input.nextDouble();
+        double c=a+b;
+        System.out.println(c);
+    }
+}
